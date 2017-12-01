935c3a316cf1bd4115b017f7839806408afd3245
Peter Sorensen <peter.sorensen@berkeley.edu>
2017-11-30 16:29:30 -0800

    added method headers

--- Reputation.sol
+++ Reputation.sol
@@ -4,24 +4,43 @@ pragma solidity ^0.4.0;
 contract Reputation {
 
     address public _agent;
-    String ownerAddress;
+    string ownerAddress;
     address _owner;
 
     struct Contractor {
         uint _repTokens;
-        String _address;
+        string _address;
     }
 
     modifier OwnerOnly() {
-        if(msg.sender == owner){
+        if(msg.sender == _owner){
+            _;
+        }
+    }
+
+    modifier AgentOnly() {
+        if(msg.sender == _agent){
             _;
         }
     }
 
     mapping(address => Contractor) AngelList;
 
-    function AddToList()
+    function AddToList() AgentOnly() {
+        // AngelList[address] = new Contractor ... 
+    }
 
+    function findAgent() OwnerOnly() {
+
+    }
+
+    function rateAgent() OwnerOnly() {
+        //agent rep ++
+    }
+
+    function rateContractor() {
+        // do stuff
+    }
 
     function Reputation(address agent){
         _owner = msg.sender;
