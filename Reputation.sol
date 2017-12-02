f68913e4059242775625c80e6b046c179f05e6c2
Brandon Griffin <brandongrifs@berkeley.edu>
2017-12-01 23:50:33 -0800

    added mitable burnable token, safemath, fixed some functions

--- Reputation.sol
+++ Reputation.sol
@@ -1,14 +1,17 @@
 pragma solidity ^0.4.0;
-
+import './Token.sol';
 
 contract Reputation {
 
+    using SafeMath for uint256;
+
     address public _agent;
     string ownerAddress;
+    //contract owner is property owner
     address _owner;
 
     struct Contractor {
-        uint _repTokens;
+        uint256 _repTokens;
         string _address;
     }
 
@@ -26,12 +29,17 @@ contract Reputation {
 
     mapping(address => Contractor) AngelList;
 
+    //only the agent can add new contractors, adds by address
+    //address should hold
     function AddToList() AgentOnly() {
+
         // AngelList[address] = new Contractor ... 
     }
 
-    function findAgent() OwnerOnly() {
-
+    //app front end can be designed to post suggested agents
+    //with their rep scores (if used) and ethereum addresses
+    function setAgent() OwnerOnly(address agent) {
+        _agent = agent;
     }
 
     function rateAgent() OwnerOnly() {
